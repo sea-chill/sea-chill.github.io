6d573b8c5ffd81a33fd680766b8aef4f9f3e926b
sea-chill <wwanner@comcast.net>
2026-03-12 21:08:19 -0400

    Add files via upload

--- PhilliesWin.js
+++ PhilliesWin.js
@@ -174,7 +174,7 @@ async function runDailyReport() {
         philliesGameTwoElement.textContent = null;
       }
 
-      const gameStatus = games[i].status.detailedState; /*this is the status of the first or second game.  
+      const gameStatus = games[i].status.abstractGameState; /*this is the status of the first or second game.  
         If there is a doubleheader, there will be two games in the games array.  
         If there is only one game, there will be one game in the games array.  
         If there are no games, there will be no games in the games array.*/
